e2ffe6f1a55d365cbd981d21eaf281e7d4190a16
Ari Fordsham <arifordsham@gmail.com>
2021-08-02 22:00:50 +0100

    Testing testing

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,11 @@
+{-# LANGUAGE LambdaCase #-}
+
+import Test.Hspec
+import System.Process
+import System.Exit
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = hspec $ do
+      it "installs a package" $ 
+        rawSystem "nix-env" ["-q", "hello"] >>=
+          (`shouldNotBe` ExitSuccess)
